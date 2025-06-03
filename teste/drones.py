28a430304b1a946d43615aeb1b2ef8a778f69297
Daniel-Alencar <danielalencar746@gmail.com>
2025-06-03 16:39:08 -0300

    teste

--- teste/drones.py
+++ teste/drones.py
@@ -1,6 +1,7 @@
 import asyncio
 from mavsdk import System
 
+
 # COMANDOS BÁSICOS
 async def connect_and_check(system_address):
     drone = System()
@@ -23,18 +24,19 @@ async def connect_and_check(system_address):
         ):
             print(f"✅ Sensores prontos em {system_address}")
             break
-
     return drone
 
 
-async def arm_takeoff_land(drone, label):
+async def arm_takeoff(drone, label):
     print(f"🚁 [{label}] Armando...")
     await drone.action.arm()
     
     print(f"🚀 [{label}] Decolando...")
     await drone.action.takeoff()
-    await asyncio.sleep(5)  
+    await asyncio.sleep(10)  
 
+
+async def land(drone, label):
     print(f"🛬 [{label}] Pousando...")
     await drone.action.land()
 
@@ -44,23 +46,39 @@ async def arm_takeoff_land(drone, label):
             print(f"✅ [{label}] Drone pousou com sucesso.")
             break
     # Simula algum tempo de operação
-    await asyncio.sleep(5)
+    await asyncio.sleep(10)
 
 
+# EXECUÇÕES
 async def main_drone1():
     # Conecte aos drones nas portas apropriadas
     drone1 = await connect_and_check("udp://:14541")
 
     # Decola e pousa ambos simultaneamente
-    await arm_takeoff_land(drone1, "Drone 1")
+    await arm_takeoff(drone1, "Drone 1")
 
 async def main_drone2():
     # Conecte aos drones nas portas apropriadas
     drone2 = await connect_and_check("udp://:14542")
 
     # Decola e pousa ambos simultaneamente
-    await arm_takeoff_land(drone2, "Drone 2")
+    await arm_takeoff(drone2, "Drone 2")
+
+async def land_drone1():
+    # Conecte aos drones nas portas apropriadas
+    drone1 = await connect_and_check("udp://:14541")
+
+    await land(drone1, "Drone 1")
+
+async def land_drone2():
+    # Conecte aos drones nas portas apropriadas
+    drone2 = await connect_and_check("udp://:14542")
+
+    await land(drone2, "Drone 2")
 
 if __name__ == "__main__":
     asyncio.run(main_drone1())
     asyncio.run(main_drone2())
+    asyncio.run(asyncio.sleep(10))
+    asyncio.run(land_drone1())
+    asyncio.run(land_drone2())
